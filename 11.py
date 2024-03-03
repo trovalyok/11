a0f605840bfc15f1c5f275ea4588bf13f40987fd
Valentyna Trokhina <trokhina.v@gmail.com>
2024-03-03 20:34:26 +0200

    Fixed a bug. Previously index i was from zero and wasn't related to j. Now in the second round every second cat starts with #2 (a[1]) instead of #1 (a[0]).

--- 11.py
+++ 11.py
@@ -22,12 +22,12 @@ def numbers_of_cats_in_hats():
         a.append(0)
 
     for j in range(1, r+1):
-        for i in range(n):
+        for i in range(1, n+1):
             if i % j == 0:
-                if a[i]:
-                    a[i] = 0
+                if a[i-1]:
+                    a[i-1] = 0
                 else:
-                    a[i] = 1
+                    a[i-1] = 1
 
     res = []
 
